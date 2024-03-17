1acc18fa848e2a25982d4a4e630bf7ab303768d1
firatdmx <firatduman91@gmail.com>
2024-03-17 10:55:07 +0300

    Texts are justified

--- app/src/main/java/com/firatdmx/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/com/firatdmx/jetpacktutorial/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,12 +53,14 @@ fun JTut(modifier: Modifier) {
         Text(
             text = stringResource(id = R.string.paragraph1),
             fontSize = 14.sp,
+            textAlign = TextAlign.Justify,
             modifier = Modifier
                 .padding(12.dp)
         )
         Text(
             text = stringResource(id = R.string.paragraph2),
             fontSize = 14.sp,
+            textAlign = TextAlign.Justify,
             modifier = Modifier
                 .padding(12.dp)
         )
